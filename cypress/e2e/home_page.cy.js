b3ebddd08c1d9c90262e9d0a4b21918be199339c
Haryo Mahendra jati <haryomahendra41@gmail.com>
2024-03-29 14:46:55 +0700

    Update home page URL in Cypress test

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('http://localhost:8080') // change URL to match your dev URL
   })
 })
